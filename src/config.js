ea1aa1e123522b2bbac58ee3cd01dbf844450d2a
Francesca-Belfiore <belfiore.francesca94@gmail.com>
2024-03-19 23:26:17 +0100

    set 3001 as default port

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 module.exports = {
   AMADEUS_CLIENT_ID: process.env.CLIENT_ID,
   AMADEUS_CLIENT_SECRET: process.env.CLIENT_SECRET,
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 3001,
   SKYSCANNER_API_KEY: process.env.SKYSCANNER_API_KEY,
   UNSPLASH_API_KEY: process.env.UNSPLASH_API_KEY
 };
